dd0b1f67f771ff638e2a5f173ea57cafaa2b7cbe
Zakaria Bendifallah <zakaria.bendifallah.external@atos.net>
2018-04-10 08:01:20 +0200

    AK: first beta version

--- keyboard/views.py
+++ keyboard/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "index.html", locals())
+
+def contact(request):
+    return render(request, "contact.html", locals())
