f572aa7c4ed1403b77893b07505be4e47e538b03
Zakaria Bendifallah <zakaria.bendifallah.external@atos.net>
2018-04-22 14:59:07 +0200

    several imrovments

--- keyboard/views.py
+++ keyboard/views.py
@@ -4,5 +4,8 @@ from django.shortcuts import render
 def index(request):
     return render(request, "index.html", locals())
 
+def translate(request):
+    return render(request, "translate.html", locals())
+
 def contact(request):
     return render(request, "contact.html", locals())
